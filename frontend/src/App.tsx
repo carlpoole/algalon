4d30850460510aac931447313c4838bceebefbb7
carlpoole <mail@carlpoole.com>
2020-10-18 15:11:56 -0500

    Front end style things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MDBCollapse, MDBContainer, MDBIcon, MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBNavItem, MDBNavLink } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  collapse: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props : Readonly<{}>) {
+      super(props);
+      this.state = {
+          collapse: false,
+      };
+
+      this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+        collapse: !this.state.collapse,
+      });
+  }
+
+  render() {
+    const navBar = { backgroundColor: '#181b24' }
+    const container = { height: 1300 }
+    return(
+      <div>
+        <Router>
+          <header>
+            <MDBNavbar style={ navBar } dark expand="md" scrolling fixed="top">
+              <MDBNavbarBrand href="/">
+                  <strong>Navbar</strong>
+              </MDBNavbarBrand>
+              <MDBNavbarToggler onClick={ this.onClick } />
+              <MDBCollapse isOpen = { this.state.collapse } navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                      <MDBNavLink to="#">Home</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                      <MDBNavLink to="#">Features</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                      <MDBNavLink to="#">Pricing</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#">Options</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#"><MDBIcon fab icon="instagram" /></MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </header>
+        </Router>
+        <MDBContainer style={ container } className="text-center mt-5 pt-5">
+          <h2>Algalon</h2>
+          <h5>This app is currently a work in progress</h5>
+          <br/>
+          <p>Check back later for more</p>
+        </MDBContainer>
+      </div>
+    );
+  }
 }
 
 export default App;
